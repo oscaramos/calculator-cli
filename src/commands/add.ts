3d06d336cdd11d4acb0a54468cafc37ce116c6fa
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-20 15:04:44 -0500

    Refactor getFilteredByMode

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,32 +1,31 @@
-import {Command} from '@oclif/command'
-import { getResult } from "../utils";
+import { Command } from "@oclif/command";
+import { getFilteredByMode } from "../utils";
 
 export default class Add extends Command {
-  static description = 'Add an set of numbers'
-  static strict = false
+  static description = "Add an set of numbers";
+  static strict = false;
   static args = [
     {
-      name: 'mode',
-      description: 'choose only these numbers',
-      required: false,
-      options: ['even', 'odd']
+      name: "mode",
+      description: "choose only these numbers",
+      required: false
     },
     {
-      name: 'numbers',
-      description: 'numbers to add, variadic',
+      name: "numbers",
+      description: "numbers to add, variadic",
     }
-  ]
+  ];
 
   static examples = [
-    '$ calculator add 2 2',
-    '$ calculator add 42 200 404',
-    '$ calculator add even 2 1 2 1',
-    '$ calculator add odd 2 1 2 1',
-  ]
+    "$ calculator add 2 2",
+    "$ calculator add 42 200 404",
+    "$ calculator add even 2 1 2 1",
+    "$ calculator add odd 2 1 2 1",
+  ];
 
   async run() {
-    const {argv} = this.parse(Add)
-    const result = getResult(argv,(x: number, y: number) => x+y);
-    this.log(`${result}`)
+    const { argv } = this.parse(Add);
+    const result = getFilteredByMode(argv).reduce((ac, x) => ac + x);
+    this.log(`${result}`);
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,16 @@
-export function getResult(argv: Array<string>, op: (x: number, y: number) => number) {
+export function getFilteredByMode(argv: Array<string>): Array<number> {
   if (argv[0] === "even" || argv[0] === "odd") {
     if (argv[0] === "even") {
       return argv.splice(1)
-        .filter(x => Number(x) % 2 === 0)
-        .reduce((ac, x) => op(ac, Number(x)), 0);
+        .map(x => Number(x))
+        .filter(x => x % 2 === 0);
     } else {
       return argv.splice(1)
-        .filter(x => Number(x) % 2 === 1)
-        .reduce((ac, x) => op(ac, Number(x)), 0);
+        .map(x => Number(x))
+        .filter(x => x % 2 === 1)
     }
   } else {
-    return argv.reduce((ac, x) => op(ac, Number(x)), 0);
+    return argv
+      .map(x => Number(x))
   }
 }
